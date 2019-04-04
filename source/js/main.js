b2f64444a904f20b35a55483c363044073a071fb
svensktutby <svensk.tut.by@gmail.com>
2019-04-04 14:13:23 +0300

    Navigation was done

--- source/js/main.js
+++ source/js/main.js
@@ -6,6 +6,7 @@
   // Add event listener for open form button
   var openFormButton = document.querySelector('.arrow-down');
   var form = document.querySelector('.form');
+  var nav = document.querySelector('.nav');
 
   if (openFormButton) {
     openFormButton.addEventListener('click', function (event) {
@@ -23,6 +24,19 @@
       } else {
         console.log('Is not valid');
       }
+    });
+  }
+
+  if (nav) {
+    nav.addEventListener('click', function (event) {
+      var target = event.target;
+
+      if (target.tagName.toLowerCase() !== 'a') {
+        return;
+      }
+
+      event.preventDefault();
+      LANDING.navigation.toggleToActiveLink(target); //LANDING.navigation from js/navigation.js
     });
   }
 }());
--- source/js/navigation.js
+++ source/js/navigation.js
@@ -1,4 +1,50 @@
-/* jshint esnext: true */
+/* global LANDING, from js/init.js */
 
-/* Form
+/* Navigation
  ******************************/
+;(function() {
+  var me = {};
+
+
+  me.toggleToActiveLink = function(target) {
+    var links = document.querySelectorAll('.nav__link');
+    var showedSection = target.dataset.link;
+
+    for (var i = 0; i < links.length; i++) {
+      if (links[i].classList.contains('nav__link--active')) {
+        links[i].classList.remove('nav__link--active');
+      }
+    }
+
+    target.classList.add('nav__link--active');
+    scrollToActiveSection(showedSection);
+  };
+
+  function scrollToActiveSection(showedSection) {
+    var section = document.querySelector('.' + showedSection);
+    var coords = section.getBoundingClientRect();
+    var animateTime = 0.4;
+
+    var timerId = setInterval(function() {
+      if (getScrollTop() < coords.top) {
+        window.scrollBy(0, 10);
+      } else {
+        clearInterval(timerId);
+      }
+    }, animateTime || 0.5);
+
+    function getScrollTop() {
+      var scrollTop = 0;
+
+      if (document.documentElement && document.documentElement.scrollTop) {
+        scrollTop = document.documentElement.scrollTop;
+      } else if (document.body) {
+        scrollTop = document.body.scrollTop;
+      }
+
+      return scrollTop;
+    }
+  }
+
+  LANDING.navigation = me;
+}());
